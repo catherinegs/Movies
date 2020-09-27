4bf9dbd862036bbbaa828c302ff818de301bce46
catherinegs <catherine.gourdinsg@gmail.com>
2020-09-27 20:29:36 +0200

    App mobile base de données films en react native

---
+++ AccueilComponent.js
@@ -0,0 +1,109 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import Svg, {
+  Defs,
+  ClipPath,
+  Rect,
+  G,
+  Image,
+  Text,
+  TSpan,
+  Path,
+  Ellipse
+} from "react-native-svg";
+/* Adobe XD React Exporter has dropped some elements not supported by react-native-svg: style, filter */
+
+const AccueilComponent = () => (
+  <Svg width={360} height={640} viewBox="0 0 360 640">
+    <Defs>
+      <ClipPath id="f">
+        <Rect width={360} height={640} />
+      </ClipPath>
+    </Defs>
+    <G id="e" className="a">
+      <Rect className="p" width={360} height={640} />
+      <G className="b" transform="translate(-2 -1)">
+        <Rect className="o" width={363} height={642} />
+        <Rect className="i" x={0.5} y={0.5} width={362} height={641} />
+      </G>
+      <G className="r" transform="matrix(1, 0, 0, 1, 0, 0)">
+        <Image
+          className="c"
+          width={360}
+          height={640}
+          xlinkHref="ComponentTMP_0-image.png"
+        />
+      </G>
+      <Text className="d" transform="translate(67 132)">
+        <TSpan x={75.175} y={45}>
+          {"HZ"}
+        </TSpan>
+        <TSpan className="e" y={45} />
+        <TSpan className="f">
+          <TSpan x={40.172} y={95}>
+            {"sans"}
+          </TSpan>
+          <TSpan className="e" y={95} />
+          <TSpan y={95}>{"limite"}</TSpan>
+        </TSpan>
+      </Text>
+      <Rect
+        className="g"
+        width={210}
+        height={42}
+        rx={21}
+        transform="matrix(-1, -0.017, 0.017, -1, 283.435, 329.758)"
+      />
+      <Text className="h" transform="translate(180 322)">
+        <TSpan x={-30.554} y={0}>
+          {"Go"}
+        </TSpan>
+      </Text>
+      <G className="q" transform="matrix(1, 0, 0, 1, 0, 0)">
+        <Rect className="i" width={360} height={24} />
+      </G>
+      <Path
+        className="j"
+        d="M12.194,10.052h0l-1.217-.839L13.16,6.174,13.1,6.118a1.855,1.855,0,0,1-.952.224,2.726,2.726,0,0,1-1.407-.406A3.251,3.251,0,0,1,9.639,4.823a3.007,3.007,0,0,1-.427-1.589A3.064,3.064,0,0,1,9.667,1.61,3.37,3.37,0,0,1,10.885.434,3.294,3.294,0,0,1,12.53,0a3.392,3.392,0,0,1,1.75.455,3.165,3.165,0,0,1,1.19,1.21,3.28,3.28,0,0,1,.42,1.61,3.831,3.831,0,0,1-.322,1.561,10.585,10.585,0,0,1-.952,1.66L12.2,10.051Zm.35-8.61a1.816,1.816,0,0,0-.9.231,1.734,1.734,0,0,0-.91,1.547,1.73,1.73,0,0,0,.252.917,1.807,1.807,0,0,0,.665.643,1.777,1.777,0,0,0,.889.231,1.836,1.836,0,0,0,1.568-.875,1.73,1.73,0,0,0,.252-.917,1.734,1.734,0,0,0-.91-1.547A1.842,1.842,0,0,0,12.544,1.442Zm-8.61,8.582a3.412,3.412,0,0,1-2.072-.665A4.277,4.277,0,0,1,.483,7.553,6.524,6.524,0,0,1,0,5.012,6.562,6.562,0,0,1,.483,2.457,4.188,4.188,0,0,1,1.862.658,3.425,3.425,0,0,1,3.934,0,3.4,3.4,0,0,1,6,.658a4.261,4.261,0,0,1,1.372,1.8,6.562,6.562,0,0,1,.483,2.555,6.524,6.524,0,0,1-.483,2.541A4.291,4.291,0,0,1,5.992,9.359,3.392,3.392,0,0,1,3.934,10.024Zm0-8.568a1.869,1.869,0,0,0-1.26.462,2.953,2.953,0,0,0-.819,1.274,5.417,5.417,0,0,0-.287,1.82,5.356,5.356,0,0,0,.287,1.813A3.025,3.025,0,0,0,2.674,8.1a1.852,1.852,0,0,0,1.26.469A1.859,1.859,0,0,0,5.187,8.1a2.966,2.966,0,0,0,.826-1.274A5.334,5.334,0,0,0,6.3,5.012a5.394,5.394,0,0,0-.287-1.82,2.91,2.91,0,0,0-.826-1.274A1.876,1.876,0,0,0,3.934,1.456ZM18.4,9.884a1.075,1.075,0,0,1-1.078-1.078,1.024,1.024,0,0,1,.315-.756,1.1,1.1,0,0,1,1.526,0,1.024,1.024,0,0,1,.315.756A1.075,1.075,0,0,1,18.4,9.884ZM33.208,9.8h-1.54V2.31L30.1,3.444l-.784-1.191L32.13.224h1.078V9.8Zm-6.287,0h-1.54V7.9H20.9v-1.2L25.256.224h1.666V6.468H28.14V7.9H26.922V9.8ZM25.3,2.506,22.624,6.468h2.758V2.506ZM18.4,4.69a1.075,1.075,0,0,1-1.078-1.078,1.078,1.078,0,1,1,1.841.763A1.047,1.047,0,0,1,18.4,4.69Z"
+        transform="translate(10.7 7.2)"
+      />
+      <Path
+        className="k"
+        d="M52.023,14h-9V.875h3V0h3V.875h3V14Zm-18,0h-14l14-14V14Zm-25,0h0L0,3.016A14.868,14.868,0,0,1,9.023,0a14.869,14.869,0,0,1,9.023,3.016L9.023,14Z"
+        transform="translate(298 5)"
+      />
+      <G transform="translate(-442 2)">
+        <Rect
+          className="i"
+          width={24}
+          height={24}
+          transform="translate(745 78)"
+        />
+        <Path
+          className="l"
+          d="M3,18H21V16H3v2Zm0-5H21V11H3v2ZM3,6V8H21V6Z"
+          transform="translate(745 78)"
+        />
+      </G>
+      <G transform="translate(168 98)">
+        <G className="m" transform="translate(-158 -30)">
+          <Ellipse className="o" cx={24.5} cy={22} rx={24.5} ry={22} />
+          <Ellipse className="i" cx={24.5} cy={22} rx={24} ry={21.5} />
+        </G>
+        <Text className="n" transform="translate(-133)">
+          <TSpan x={-14.802} y={0}>
+            {"HZ"}
+          </TSpan>
+        </Text>
+      </G>
+    </G>
+  </Svg>
+);
+
+export default AccueilComponent;
